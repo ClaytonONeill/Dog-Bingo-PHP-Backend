883679842a3fdd14ab0f22d007843ff3ce569b30
Clayton O'Neill <claytononeill@Claytons-MacBook-Pro.local>
2020-04-22 14:21:41 -0400

    the

---
+++ api/controllers/dogs.php
@@ -0,0 +1,6 @@
+<?php
+include_once __DIR__ . '/../models/quiz.php';
+header('Content-Type: application/json');
+
+
+ ?>
---
+++ api/models/dogupload.php
@@ -0,0 +1,23 @@
+<?php
+
+if(getenv('DATABASE_URL')){
+    $connectionConfig = parse_url(getenv('DATABASE_URL'));
+    $host = $connectionConfig['host'];
+    $user = $connectionConfig['user'];
+    $password = $connectionConfig['pass'];
+    $port = $connectionConfig['port'];
+    $dbname = trim($connectionConfig['path'],'/');
+    $dbconn = pg_connect(
+        "host=".$host." ".
+        "user=".$user." ".
+        "password=".$password." ".
+        "port=".$port." ".
+        "dbname=".$dbname
+    );
+} else {
+    $dbconn = pg_connect("host=localhost dbname=dog-bingo");
+
+
+
+
+ ?>
