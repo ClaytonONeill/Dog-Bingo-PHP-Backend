f5be78a5f0dfc086b821ee688bafc84adcf9fe1d
Clayton O'Neill <claytononeill@Claytons-MacBook-Pro.local>
2020-04-22 17:35:04 -0400

    testing

--- api/models/dogupload.php
+++ api/models/dogupload.php
@@ -20,40 +20,40 @@ if(getenv('DATABASE_URL')){
     $dbconn = pg_connect("host=localhost dbname=dog-bingo");
 
 
-class DogUpload {
-  public $id;
-  public $breed;
-  public $image;
-  public $location;
-
-  public function __construct($id, $breed, $image, $location) {
-    $this->id = $id;
-    $this->breed = $breed;
-    $this->image = $image;
-    $this->location = $location;
-  }
-}
-
-class DogUploads {
-  static function all() {
-    $uploads = array();
-
-    $results = pg_query("SELECT * FROM dogupload");
-
-    $row_object = pg_fetch_object($results);
-    while($row_object){
-      $new_upload = new DogUpload(
-        intval($row_object->id),
-        $row_object->breed,
-        $row_object->image,
-        $row_object->location
-      );
-
-      $uploads[] = $new_upload;
-      $row_object = pg_fetch_object($results);
-    }
-    return $uploads;
-  }
-}
+// class DogUpload {
+//   public $id;
+//   public $breed;
+//   public $image;
+//   public $location;
+//
+//   public function __construct($id, $breed, $image, $location) {
+//     $this->id = $id;
+//     $this->breed = $breed;
+//     $this->image = $image;
+//     $this->location = $location;
+//   }
+// }
+//
+// class DogUploads {
+//   static function all() {
+//     $uploads = array();
+//
+//     $results = pg_query("SELECT * FROM dogupload");
+//
+//     $row_object = pg_fetch_object($results);
+//     while($row_object){
+//       $new_upload = new DogUpload(
+//         intval($row_object->id),
+//         $row_object->breed,
+//         $row_object->image,
+//         $row_object->location
+//       );
+//
+//       $uploads[] = $new_upload;
+//       $row_object = pg_fetch_object($results);
+//     }
+//     return $uploads;
+//   }
+// }
 
  ?>
